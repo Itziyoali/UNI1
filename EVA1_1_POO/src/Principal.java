d3752a8289020a25bb5b290741f9a20fe3890004
Itziyoali <43557041+Itziyoali@users.noreply.github.com>
2018-11-06 20:14:14 -0700

    Add files via upload

---
+++ EVA1_1_POO/src/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * HOLA MUNDO. PRACTICA PARA VERIFICAR EL FUNCIONAMIENTO DE LOS EQUIPOS DE LABORATORIOS.
+ */
+
+/**
+ *
+ * @author Gema Itziyoali Trevizo Granados 18550294
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo");
+        System.out.println("Programación Orientada a Objetos");
+    }
+    
+}
